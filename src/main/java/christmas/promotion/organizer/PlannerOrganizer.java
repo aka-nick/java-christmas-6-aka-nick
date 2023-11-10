ef3f35c5b8198fe26ff6c213a82239d0eae9cafd
aka-nick <bechalrang@gmail.com>
2023-11-10 18:04:41 +0900

    추가(PlannerOrganizer) : 예약일자 질문
    예약일자를 질문하고 답변을 받는 코드 구현함
    '방문일자'와 '예약일자'가 동일한 개념이라는 것을 문서에 추가함
    
    PlannerOrganizer
    README

--- src/main/java/christmas/promotion/organizer/PlannerOrganizer.java
+++ src/main/java/christmas/promotion/organizer/PlannerOrganizer.java
@@ -1,9 +1,24 @@
 package christmas.promotion.organizer;
 
+import christmas.promotion.organizer.io.Input;
+import christmas.promotion.organizer.io.Output;
+
 public class PlannerOrganizer {
 
+    private final Output output;
+    private final Input input;
+
+    public PlannerOrganizer(Input input, Output output) {
+        this.input = input;
+        this.output = output;
+    }
+
     public void run() {
-        System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
+        output.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
+
+        output.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+        int reservationDate = input.number();
+
     }
 
 }
