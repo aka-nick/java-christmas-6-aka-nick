2be714f3a59930fb1dcd605dddaef41225c89cc8
aka-nick <bechalrang@gmail.com>
2023-11-10 20:47:25 +0900

    추가(Food) : 음식 객체 속성
    생각난 음식 객체의 속성을 미리 추가해놓음
    
    Food

--- src/main/java/christmas/promotion/collborator/shop/Food.java
+++ src/main/java/christmas/promotion/collborator/shop/Food.java
@@ -1,5 +1,15 @@
 package christmas.promotion.collborator.shop;
 
+import christmas.promotion.collborator.generic.Money;
+
 public class Food {
+
+    private final String name;
+    private final Money price;
+
+    public Food(String name, long amount) {
+        this.name = name;
+        this.price = Money.won(amount);
+    }
 
 }
