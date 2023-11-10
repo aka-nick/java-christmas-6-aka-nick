23cd54a1fb8f0f4a41dbfcd8edec653787549e60
aka-nick <bechalrang@gmail.com>
2023-11-10 16:19:23 +0900

    추가(Output) : 출력 기능 인터페이스
    출력 기능을 구현할 인터페이스 구성함
    기본 구현체인 ConsoleOutput 구현함
    
    Output
    ConsoleOutput

---
+++ src/main/java/christmas/promotion/organizer/io/Output.java
@@ -0,0 +1,11 @@
+package christmas.promotion.organizer.io;
+
+public interface Output {
+
+    void print(final Object message);
+
+    void println();
+
+    void println(final Object message);
+
+}
