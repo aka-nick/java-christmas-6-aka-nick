05b2399509fa0604408c6515ead060cf9aa62c5b
aka-nick <bechalrang@gmail.com>
2023-11-10 17:58:46 +0900

    추가(Dependency) : input() 구현체 획득 기능
    Input 클래스의 구현체를 획득할 input() 메서드를 구현함
    
    Dependency

--- src/main/java/christmas/config/Dependency.java
+++ src/main/java/christmas/config/Dependency.java
@@ -1,11 +1,26 @@
 package christmas.config;
 
+import christmas.promotion.external.io.ConsoleInput;
+import christmas.promotion.external.io.ConsoleOutput;
 import christmas.promotion.organizer.PlannerOrganizer;
+import christmas.promotion.organizer.io.Input;
+import christmas.promotion.organizer.io.Output;
 
 public class Dependency {
 
+    private static final Input INPUT = new ConsoleInput();
+    private static final Output OUTPUT = new ConsoleOutput();
+
     public static PlannerOrganizer promotionPlanner() {
-        return new PlannerOrganizer();
+        return new PlannerOrganizer(output());
+    }
+
+    private static Input input() {
+        return INPUT;
+    }
+
+    private static Output output() {
+        return OUTPUT;
     }
 
 }
