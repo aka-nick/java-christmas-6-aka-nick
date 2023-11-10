4db0e518a04de14d31766e6126a45edde6a76166
aka-nick <bechalrang@gmail.com>
2023-11-10 18:12:34 +0900

    추가(PlannerOrganizer) : 주문내용 입력받는 기능
    사용자의 주문내역 전체를 입력받는 기능을 구현함
    
    PlannerOrganizer

--- src/main/java/christmas/promotion/organizer/PlannerOrganizer.java
+++ src/main/java/christmas/promotion/organizer/PlannerOrganizer.java
@@ -20,7 +20,7 @@ public class PlannerOrganizer {
         int reservationDate = input.number();
 
         output.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
-
+        String orderedFoodAndQuantity = input.string();
     }
 
 }
