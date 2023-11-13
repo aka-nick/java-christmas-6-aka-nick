1e919b2ea37df9c182ca4eb676d968379d05e325
aka-nick <bechalrang@gmail.com>
2023-11-13 22:07:42 +0900

    추가(OrderManager): 음식 주문 기능
    커스텀 예외 처리 가능한 음식 주문 기능 코드 구현함
    
    OrderManager

---
+++ src/main/java/christmas/promotion/organizer/viewer/OrderManager.java
@@ -0,0 +1,35 @@
+package christmas.promotion.organizer.viewer;
+
+import static christmas.promotion.enums.organizer.viewer.ReservationManagerMessage.TAKE_ORDERS;
+
+import christmas.promotion.collborator.order.Order;
+import christmas.promotion.collborator.order.Orders;
+import christmas.promotion.exception.InvalidReservationOrderException;
+import christmas.promotion.organizer.io.Input;
+import christmas.promotion.organizer.io.InteractionRepeatable;
+import christmas.promotion.organizer.io.Output;
+import java.util.List;
+
+public class OrderManager implements InteractionRepeatable {
+
+    private final Input input;
+    private final Output output;
+
+    public OrderManager(Input input, Output output) {
+        this.input = input;
+        this.output = output;
+    }
+
+    public Orders takeOrders() {
+        return supplyInteractionWithCustomException(() -> {
+            output.println(TAKE_ORDERS);
+            List<String> reservations = input.strings(",");
+            return new Orders(reservations.stream()
+                    .map(reservation -> reservation.split("-"))
+                    .map(foodElements ->
+                            Order.place(foodElements[0], Integer.parseInt(foodElements[1])))
+                    .toList());
+        }, new InvalidReservationOrderException());
+    }
+
+}
