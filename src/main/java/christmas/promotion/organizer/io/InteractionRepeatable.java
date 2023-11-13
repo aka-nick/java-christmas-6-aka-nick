25433203f036967e4dadcd9598023bc401ccc30f
aka-nick <bechalrang@gmail.com>
2023-11-13 16:06:03 +0900

    추가(InteractionRepeatable): 반복 가능한 입력 인터페이스
    사용자와의 대화(Interaction) 시 예외값을 입력하더라도 재입력을 받을 수 있는 기능을 제공하는 인터페이스 추가함
    
    InteractionRepeatable

---
+++ src/main/java/christmas/promotion/organizer/io/InteractionRepeatable.java
@@ -0,0 +1,32 @@
+package christmas.promotion.organizer.io;
+
+import christmas.config.Dependency;
+import christmas.promotion.enums.GlobalMessage;
+import java.util.function.Supplier;
+
+public interface InteractionRepeatable {
+
+    Output output = Dependency.output();
+
+    default void runInteraction(final Runnable runnable) {
+        while (true) {
+            try {
+                runnable.run();
+                break;
+            } catch (IllegalArgumentException e) {
+                output.println(GlobalMessage.ERROR_HEADER.get() + e.getMessage());
+            }
+        }
+    }
+
+    default <T> T supplyInteraction(final Supplier<T> supplier) {
+        while (true) {
+            try {
+                return supplier.get();
+            } catch (IllegalArgumentException e) {
+                output.println(GlobalMessage.ERROR_HEADER.get() + e.getMessage());
+            }
+        }
+    }
+
+}
