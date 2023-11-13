9ed4fe66e06b8b159a3d2081fc59bde63099245d
aka-nick <bechalrang@gmail.com>
2023-11-13 22:06:42 +0900

    리팩토링(ReservationManager): 날짜 입력 기능 개선
    날짜 입력 기능 수행 시 커스텀 예외 처리 가능한 코드로 리팩토링 함
    
    ReservationManager
    ReservationManagerMessage

--- src/main/java/christmas/promotion/organizer/viewer/ReservationManager.java
+++ src/main/java/christmas/promotion/organizer/viewer/ReservationManager.java
@@ -1,19 +1,14 @@
 package christmas.promotion.organizer.viewer;
 
-import static christmas.promotion.enums.organizer.viewer.ReservationManagerMessage.*;
+import static christmas.promotion.enums.organizer.viewer.ReservationManagerMessage.ASK_RESERVATION_DATE;
+import static christmas.promotion.enums.organizer.viewer.ReservationManagerMessage.SAY_GREETING;
 
-import christmas.promotion.collborator.calendar.Calendar;
 import christmas.promotion.collborator.calendar.Date;
-import christmas.promotion.collborator.order.Order;
-import christmas.promotion.collborator.order.OrderFood;
 import christmas.promotion.collborator.order.Orders;
 import christmas.promotion.exception.InvalidReservationDateException;
-import christmas.promotion.exception.InvalidReservationOrderException;
-import christmas.promotion.organizer.calendar.ReservationDate;
 import christmas.promotion.organizer.io.Input;
 import christmas.promotion.organizer.io.InteractionRepeatable;
 import christmas.promotion.organizer.io.Output;
-import java.util.List;
 
 public class ReservationManager implements InteractionRepeatable {
 
@@ -25,45 +20,20 @@ public class ReservationManager implements InteractionRepeatable {
         this.output = output;
     }
 
-
-    public void sayGreet() {
-        output.println(SAY_GREET);
+    public void sayGreeting() {
+        output.println(SAY_GREETING);
     }
 
     public Date askReservationDate() {
-        final Integer reservationDate = supplyInteraction(() -> {
+        return supplyInteractionWithCustomException(() -> {
             output.println(ASK_RESERVATION_DATE);
-            try {
-                return input.number();
-            } catch (IllegalArgumentException e) {
-                throw new InvalidReservationDateException(e);
-            }
-        });
-
-        return Calendar.findDate(reservationDate);
+            return Date.of(input.number());
+        }, new InvalidReservationDateException());
     }
 
-    public Orders takeOrders() {
-        return supplyInteraction(() -> {
-            try {
-                output.println(TAKE_ORDERS);
-                List<String> reservations = input.strings(",");
-                return new Orders(reservations.stream()
-                        .map(reservation -> reservation.split("-"))
-                        .map(foodElements -> Order.place(convertToOrderFood(foodElements)))
-                        .toList());
-            } catch (IllegalArgumentException e) {
-                throw new InvalidReservationOrderException(e);
-            }
-        });
-    }
+    public void announceBenefitPreview(Date reservationDate, Orders orders) {
+        output.println("12월 " + reservationDate.date() + "일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!");
 
-    private OrderFood convertToOrderFood(String[] fromElement) {
-        try {
-            return new OrderFood(fromElement[0], Integer.parseInt(fromElement[1]));
-        } catch (NumberFormatException e) {
-            throw new InvalidReservationOrderException(e);
-        }
     }
 
 }
