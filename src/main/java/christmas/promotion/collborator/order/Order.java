f85d47b2b9a4bea3ab6fa1adda263df4354a682d
aka-nick <bechalrang@gmail.com>
2023-11-13 12:44:15 +0900

    추가(Order) : 주문 객체 추가
    주문건Orders와 주문건에 속한 각 음식주문Order을 표현하는 객체를 구현함
    
    Orders
    Order

--- src/main/java/christmas/promotion/collborator/order/Order.java
+++ src/main/java/christmas/promotion/collborator/order/Order.java
@@ -1,5 +1,29 @@
 package christmas.promotion.collborator.order;
 
+import christmas.promotion.collborator.menu.Food;
+import christmas.promotion.collborator.menu.Menu;
+
 public class Order {
+
+    private final Food food;
+    private final int amount;
+
+    private Order(Food food, int amount) {
+        this.food = food;
+        this.amount = amount;
+    }
+
+    public static Order place(String[] foodElements) {
+        // TODO : 컨버트 로직 같은건 나중에 전용뷰가 생기면 그리로 옮겨져야 한다, 아니면 전용 매퍼를 만들던지
+        return new Order(Menu.findBy(foodElements[0]), Integer.parseInt(foodElements[1]));
+    }
+
+    public Food getFood() {
+        return food;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
 
 }
---
+++ src/main/java/christmas/promotion/collborator/order/Orders.java
@@ -0,0 +1,7 @@
+package christmas.promotion.collborator.order;
+
+import java.util.List;
+
+public record Orders(List<Order> orders) {
+
+}
