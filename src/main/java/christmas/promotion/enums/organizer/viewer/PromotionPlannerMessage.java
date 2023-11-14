e3c30a183ca9307aa80c1ad8433fd3ff95cd1e55
aka-nick <bechalrang@gmail.com>
2023-11-14 15:19:31 +0900

    리팩토링(PromotionPlanner): 메시지 생성 책임을 메시지 객체로 이동
    PromotionPlanner에게 있었던 메시지 생성 책임이 너무 많아져서, 메시지를 담당하고 있던 PromotionPlannerMessage에게 메시지 생성 로직을 전부 이전했음
    
    PromotionPlanner
    PromotionPlannerMessage

--- src/main/java/christmas/promotion/enums/organizer/viewer/PromotionPlannerMessage.java
+++ src/main/java/christmas/promotion/enums/organizer/viewer/PromotionPlannerMessage.java
@@ -1,5 +1,9 @@
 package christmas.promotion.enums.organizer.viewer;
 
+import christmas.promotion.collborator.calendar.PromotionBadge;
+import christmas.promotion.enums.GlobalMessage;
+import java.util.List;
+
 public enum PromotionPlannerMessage {
 
     ANNOUNCE_PREVIEW_BEFORE("12월 "),
@@ -23,6 +27,53 @@ public enum PromotionPlannerMessage {
 
     public String get() {
         return message;
+    }
+
+    public static String getPreviewMessage(int date) {
+        return ANNOUNCE_PREVIEW_BEFORE.get() + date + ANNOUNCE_PREVIEW_AFTER.get()
+                + GlobalMessage.BLANK_AND_NEW_LINE.get();
+    }
+
+    public static String getOrderFoodNameMessage(List<String> orderedFood) {
+        return TITLE_ORDER_FOOD_NAME.get() + GlobalMessage.NEW_LINE.get()
+                + String.join(GlobalMessage.NEW_LINE.get(), orderedFood)
+                + GlobalMessage.BLANK_AND_NEW_LINE.get();
+    }
+
+    public static String getTotalPriceMessage(int totalPrice) {
+        return TITLE_AMOUNT_OF_BEFORE_DISCOUNT.get() + GlobalMessage.NEW_LINE.get()
+                + totalPrice
+                + GlobalMessage.BLANK_AND_NEW_LINE.get();
+    }
+
+    public static String getGiveawayHistoryMessage(String benefitsHistory) {
+        return TITLE_GIVEAWAY.get() + GlobalMessage.NEW_LINE.get()
+                + benefitsHistory
+                + GlobalMessage.BLANK_AND_NEW_LINE.get();
+    }
+
+    public static String getBenefitsMessage(String benefitsMessage) {
+        return TITLE_BENEFITS.get() + GlobalMessage.NEW_LINE.get()
+                + benefitsMessage
+                + GlobalMessage.BLANK_AND_NEW_LINE.get();
+    }
+
+    public static String getTotalAmountOfApplyPromotionMessage(Integer amountOfTotal) {
+        return TITLE_AMOUNT_OF_BENEFITS.get() + GlobalMessage.NEW_LINE.get()
+                + MINUS_AMOUNT_OF_BENEFITS + amountOfTotal + WON_AMOUNT_OF_BENEFITS.get()
+                + GlobalMessage.BLANK_AND_NEW_LINE.get();
+    }
+
+    public static String getFinalAmountMessage(int totalOrderPrice, int totalBenefitAmount) {
+        return TITLE_AMOUNT_OF_EXPECTED.get()
+                + (totalOrderPrice - totalBenefitAmount) + WON_AMOUNT_OF_BENEFITS.get()
+                + GlobalMessage.BLANK_AND_NEW_LINE.get();
+    }
+
+    public static String getPromotionBadgeMessage(int totalBenefitAmount) {
+        return TITLE_PROMOTION_BADGE.get()
+                + PromotionBadge.findPromotionBadgeBy(totalBenefitAmount)
+                + GlobalMessage.BLANK_AND_NEW_LINE.get();
     }
 
     @Override
--- src/main/java/christmas/promotion/organizer/viewer/PromotionPlanner.java
+++ src/main/java/christmas/promotion/organizer/viewer/PromotionPlanner.java
@@ -1,24 +1,19 @@
 package christmas.promotion.organizer.viewer;
 
-import static christmas.promotion.enums.organizer.viewer.PromotionPlannerMessage.ANNOUNCE_PREVIEW_AFTER;
-import static christmas.promotion.enums.organizer.viewer.PromotionPlannerMessage.ANNOUNCE_PREVIEW_BEFORE;
-import static christmas.promotion.enums.organizer.viewer.PromotionPlannerMessage.MINUS_AMOUNT_OF_BENEFITS;
-import static christmas.promotion.enums.organizer.viewer.PromotionPlannerMessage.TITLE_AMOUNT_OF_BEFORE_DISCOUNT;
-import static christmas.promotion.enums.organizer.viewer.PromotionPlannerMessage.TITLE_AMOUNT_OF_BENEFITS;
-import static christmas.promotion.enums.organizer.viewer.PromotionPlannerMessage.TITLE_AMOUNT_OF_EXPECTED;
-import static christmas.promotion.enums.organizer.viewer.PromotionPlannerMessage.TITLE_BENEFITS;
-import static christmas.promotion.enums.organizer.viewer.PromotionPlannerMessage.TITLE_GIVEAWAY;
-import static christmas.promotion.enums.organizer.viewer.PromotionPlannerMessage.TITLE_ORDER_FOOD_NAME;
-import static christmas.promotion.enums.organizer.viewer.PromotionPlannerMessage.TITLE_PROMOTION_BADGE;
-import static christmas.promotion.enums.organizer.viewer.PromotionPlannerMessage.WON_AMOUNT_OF_BENEFITS;
+import static christmas.promotion.enums.organizer.viewer.PromotionPlannerMessage.getBenefitsMessage;
+import static christmas.promotion.enums.organizer.viewer.PromotionPlannerMessage.getFinalAmountMessage;
+import static christmas.promotion.enums.organizer.viewer.PromotionPlannerMessage.getGiveawayHistoryMessage;
+import static christmas.promotion.enums.organizer.viewer.PromotionPlannerMessage.getOrderFoodNameMessage;
+import static christmas.promotion.enums.organizer.viewer.PromotionPlannerMessage.getPreviewMessage;
+import static christmas.promotion.enums.organizer.viewer.PromotionPlannerMessage.getPromotionBadgeMessage;
+import static christmas.promotion.enums.organizer.viewer.PromotionPlannerMessage.getTotalAmountOfApplyPromotionMessage;
+import static christmas.promotion.enums.organizer.viewer.PromotionPlannerMessage.getTotalPriceMessage;
 
 import christmas.promotion.collborator.calendar.Calendar;
 import christmas.promotion.collborator.calendar.Date;
-import christmas.promotion.collborator.calendar.PromotionBadge;
 import christmas.promotion.collborator.calendar.Promotions;
 import christmas.promotion.collborator.calendar.benefit.BenefitAmount;
 import christmas.promotion.collborator.order.Orders;
-import christmas.promotion.enums.GlobalMessage;
 import christmas.promotion.organizer.io.Output;
 
 public class PromotionPlanner {
@@ -31,76 +26,20 @@ public class PromotionPlanner {
 
     public void announceBenefitPreview(Date reservationDate, Orders orders) {
         BenefitAmount benefits = getBenefitAmount(reservationDate, orders);
-        output.println(new StringBuilder()
-                .append(announcePreview(reservationDate))
-                .append(findOrderedFoodsNameFrom(orders))
-                .append(calculateTotalPriceFrom(orders))
-                .append(showGiveawayHistoryFrom(benefits))
-                .append(showBenefitsFrom(benefits))
-                .append(calculateTotalAmountOfApplyPromotionFrom(benefits))
-                .append(calculateFinalAmount(orders, benefits))
-                .append(calculatePromotionBadge(benefits)));
+
+        output.println(getPreviewMessage(reservationDate.date())
+                + getOrderFoodNameMessage(orders.findAllOrderedFood())
+                + getTotalPriceMessage(orders.calculateTotalPrice())
+                + getGiveawayHistoryMessage(benefits.askResultOfGiveaway())
+                + getBenefitsMessage(benefits.findBenefitMessages())
+                + getTotalAmountOfApplyPromotionMessage(benefits.amountOfTotalBenefits())
+                + getFinalAmountMessage(orders.calculateTotalPrice(), benefits.amountOfTotalBenefits())
+                + getPromotionBadgeMessage(benefits.amountOfTotalBenefits()));
     }
 
     private static BenefitAmount getBenefitAmount(Date reservationDate, Orders orders) {
         Promotions promotionsOfReservationDate = Calendar.findPromotionsBy(reservationDate, orders);
         return promotionsOfReservationDate.askBenefitAmount();
-    }
-
-    private StringBuilder announcePreview(Date reservationDate) {
-        return new StringBuilder()
-                .append(ANNOUNCE_PREVIEW_BEFORE).append(reservationDate.date()).append(ANNOUNCE_PREVIEW_AFTER)
-                .append(GlobalMessage.BLANK_AND_NEW_LINE.get());
-    }
-
-    private StringBuilder findOrderedFoodsNameFrom(Orders orders) {
-        return new StringBuilder()
-                .append(TITLE_ORDER_FOOD_NAME).append(GlobalMessage.NEW_LINE.get())
-                .append(String.join(GlobalMessage.NEW_LINE.get(), orders.findAllOrderedFood()))
-                .append(GlobalMessage.BLANK_AND_NEW_LINE.get());
-    }
-
-    private StringBuilder calculateTotalPriceFrom(Orders orders) {
-        return new StringBuilder()
-                .append(TITLE_AMOUNT_OF_BEFORE_DISCOUNT).append(GlobalMessage.NEW_LINE.get())
-                .append(orders.calculateTotalPrice())
-                .append(GlobalMessage.BLANK_AND_NEW_LINE.get());
-    }
-
-    private StringBuilder showGiveawayHistoryFrom(BenefitAmount benefits) {
-        return new StringBuilder()
-                .append(TITLE_GIVEAWAY).append(GlobalMessage.NEW_LINE.get())
-                .append(benefits.askResultOfGiveaway())
-                .append(GlobalMessage.BLANK_AND_NEW_LINE.get());
-    }
-
-    private StringBuilder showBenefitsFrom(BenefitAmount benefits) {
-        StringBuilder message = new StringBuilder();
-        message.append(TITLE_BENEFITS).append(GlobalMessage.NEW_LINE.get());
-
-        return message.append(benefits.findBenefitMessages())
-                .append(GlobalMessage.BLANK_AND_NEW_LINE.get());
-    }
-
-    private StringBuilder calculateTotalAmountOfApplyPromotionFrom(BenefitAmount benefits) {
-        return new StringBuilder()
-                .append(TITLE_AMOUNT_OF_BENEFITS).append(GlobalMessage.NEW_LINE.get())
-                .append(MINUS_AMOUNT_OF_BENEFITS).append(benefits.amountOfTotalBenefits()).append(WON_AMOUNT_OF_BENEFITS)
-                .append(GlobalMessage.BLANK_AND_NEW_LINE.get());
-    }
-
-    private StringBuilder calculateFinalAmount(Orders orders, BenefitAmount benefits) {
-        return new StringBuilder()
-                .append(TITLE_AMOUNT_OF_EXPECTED)
-                .append(orders.calculateTotalPrice() - benefits.amountOfTotalBenefits()).append("원")
-                .append(GlobalMessage.BLANK_AND_NEW_LINE.get());
-    }
-
-    private StringBuilder calculatePromotionBadge(BenefitAmount benefits) {
-        return new StringBuilder()
-                .append(TITLE_PROMOTION_BADGE)
-                .append(PromotionBadge.findPromotionBadgeBy(benefits.amountOfTotalBenefits()))
-                .append(GlobalMessage.BLANK_AND_NEW_LINE.get());
     }
 
 }
