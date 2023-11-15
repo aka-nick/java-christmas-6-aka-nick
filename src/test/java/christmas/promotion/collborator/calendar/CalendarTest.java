ab38e8dfae1cf1642dd20ecd0cfd50da6b508774
aka-nick <bechalrang@gmail.com>
2023-11-15 11:33:01 +0900

    추가(CalendarTest): Calendar의 테스트 클래스
     Calendar의 공개 메서드를 테스트하는 테스트 클래스를 구현함
    
    CalendarTest

---
+++ src/test/java/christmas/promotion/collborator/calendar/CalendarTest.java
@@ -0,0 +1,48 @@
+package christmas.promotion.collborator.calendar;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import christmas.promotion.collborator.order.Order;
+import christmas.promotion.collborator.order.Orders;
+import java.util.List;
+import java.util.Optional;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class CalendarTest {
+
+    public static final int NUMBER_OF_VALID_DATE = 24;
+
+    @Test
+    void 정상적으로날짜객체를조회해올수있다() {
+        Optional<Date> validDate = Calendar.findDate(NUMBER_OF_VALID_DATE);
+
+        assertThat(validDate.isPresent()).isTrue();
+        assertThat(validDate.get().date()).isEqualTo(NUMBER_OF_VALID_DATE);
+    }
+
+    @Test
+    void 최소날짜보다작은비정상적인날짜조회시_빈옵셔널반환() {
+        assertThat(Calendar.findDate(0).isEmpty()).isTrue();
+    }
+
+    @Test
+    void 최대날짜보다큰비정상적인날짜조회시_빈옵셔널반환() {
+        assertThat(Calendar.findDate(300).isEmpty()).isTrue();
+    }
+
+    @Test
+    void 정상적인날짜로해당일자의프로모션조회가능() {
+        Orders orders = new Orders(List.of(Order.place("크리스마스파스타", 4)));
+
+        Promotions expected = new Promotions(Calendar.findDate(NUMBER_OF_VALID_DATE).get(), orders);
+        Promotions actual = Calendar.findPromotionsBy(Calendar.findDate(NUMBER_OF_VALID_DATE).get(), orders);
+
+        assertThat(actual.askBenefitAmount().amountOfTotalBenefits())
+                .isEqualTo(expected.askBenefitAmount().amountOfTotalBenefits());
+    }
+
+}
