2a8591d6c154266ec9c0ed625959ea797640c6d8
aka-nick <bechalrang@gmail.com>
2023-11-15 13:59:43 +0900

    추가(Won): 화폐 객체 추가
    금액의 표현에 있어서 정수타입(int, Integer) 대신 사용할 값객체 타입을 정의함
    
    Won

--- src/main/java/christmas/promotion/collborator/generic/Won.java
+++ src/main/java/christmas/promotion/collborator/generic/Won.java
@@ -1,17 +1,39 @@
 package christmas.promotion.collborator.generic;
 
 import java.math.BigInteger;
+import java.text.NumberFormat;
+import java.util.Locale;
 
-public class Won {
+public record Won(BigInteger amount) {
 
-    private final BigInteger amount;
+    public static final String UNIT_OF_WON = "원";
 
-    private Won(BigInteger amount) {
-        this.amount = amount;
+    public static Won of (BigInteger amount) {
+        return new Won(amount);
+    }
+    public static Won of(long amount) {
+        return new Won(BigInteger.valueOf(amount));
+    }
+    public static Won of(int amount) {
+        return new Won(BigInteger.valueOf(amount));
     }
 
-    public static Won won(long amount) {
-        return new Won(BigInteger.valueOf(amount));
+    public int intValue() {
+        return amount.intValue();
+    }
+
+    public Won minus(Won other) {
+        return Won.of(this.amount.subtract(other.amount));
+    }
+
+    public String format() {
+        return NumberFormat.getNumberInstance(Locale.KOREAN).format(amount) + UNIT_OF_WON;
+    }
+
+    @Override
+    public String toString() {
+        return format();
+
     }
 
 }
